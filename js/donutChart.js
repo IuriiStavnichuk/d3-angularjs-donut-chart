b1cdd620b2b13662cd83133530e8d72699189c58
IuriiStavnichuk <info@fastforwardpro.com>
2014-09-24 09:54:25 -0400

    Code refactoring

--- js/donutChart.js
+++ js/donutChart.js
@@ -1,6 +1,6 @@
-angular.module('MainWebApp', [ 'ngResource','payza.donutChart'])
+angular.module('MainWebApp', [ 'ngResource','donutChart']);
 
-angular.module('payza.donutChart',[])
+angular.module('donutChart',[])
     .directive("donutChart", function ( $resource ) {
         return {
             restrict: "E",
@@ -9,40 +9,37 @@ angular.module('payza.donutChart',[])
                 pathToJson:'='
             },
             link: function ( $scope) {
+                $resource ($scope.pathToJson).get(function (data) {
 
-                        $resource ($scope.pathToJson).get(function (data) {
+                    for (var i in data.Results) {
 
-                            console.log("data >>", data)
-                            for (var i in data.Results) {
-
-                                data.Results[i].CurrencyAbbreviation = data.Results[i].CurrencyAbbreviation  || '';
-                                data.Results[i].CurrencySymbol = data.Results[i].CurrencySymbol  || '';
-                                data.Results[i].TimeInterval = data.Results[i].TimeInterval  || '';
-                                data.Results[i].Unit = data.Results[i].Unit  || '';
+                        data.Results[i].CurrencyAbbreviation = data.Results[i].CurrencyAbbreviation  || '';
+                        data.Results[i].CurrencySymbol = data.Results[i].CurrencySymbol  || '';
+                        data.Results[i].TimeInterval = data.Results[i].TimeInterval  || '';
+                        data.Results[i].Unit = data.Results[i].Unit  || '';
 
 
-                                $scope.title=data.Results[i].Title;
+                        $scope.title=data.Results[i].Title;
 
-                                $scope.totalAmount=data.Results[i].CurrencySymbol+data.Results[i].TotalAmount;
-                                $scope.timeInterval=data.Results[i].TimeInterval;
+                        $scope.totalAmount=data.Results[i].CurrencySymbol+data.Results[i].TotalAmount;
+                        $scope.timeInterval=data.Results[i].TimeInterval;
 
-                                var chartData=[
-                                    {'name': 'amount1','value': data.Results[i].Amount},
-                                    {'name': 'amount2','value': data.Results[i].TotalAmount-data.Results[i].Amount}
-                                ]
+                        var chartData=[
+                            {'name': 'amount1','value': data.Results[i].Amount},
+                            {'name': 'amount2','value': data.Results[i].TotalAmount-data.Results[i].Amount}
+                        ];
 
-                                var width = 220,
-                                    height = 84,
-                                    radius = Math.min(width, height) / 2;
+                        var width = 220,
+                            height = 84,
+                            radius = Math.min(width, height) / 2;
 
-                                var color = d3.scale.ordinal()
-                                    .range(["#f04d4d", "#e7eaf0"]);
+                        var color = d3.scale.ordinal()
+                            .range(["#f04d4d", "#e7eaf0"]);
 
-                                var arc = d3.svg.arc()
+                        var arc = d3.svg.arc()
                                 .outerRadius(radius - 79)
                                 .innerRadius(radius - 84)
                             ;
-
                         var pie = d3.layout.pie()
                             .sort(null)
                             .value(function(d) { return d.value; });
@@ -79,7 +76,6 @@ angular.module('payza.donutChart',[])
                                 .attr("dx", "52")
                                 .attr("dy", "21")
                             ;
-
                         chartInfo.append("tspan")
                             .attr("class", "donut-chart-total-amount")
                             .text( $scope.totalAmount )
@@ -96,13 +92,11 @@ angular.module('payza.donutChart',[])
                             .duration(1500)
                             .style("fill-opacity", 1 )
                         ;
-
                         var g = svg.selectAll(".arc")
                             .data(pie(chartData))
                             .enter().append("g")
                             .attr("transform","rotate(180)")
                             .attr("class", "donut-chart-arc");
-
                         g.append("path")
                             .style("fill", function(d) { return color(d.data.name); })
                             .transition()
@@ -110,14 +104,13 @@ angular.module('payza.donutChart',[])
                             .delay(function(d, i) {return i * 50; })
                             .attrTween("d", tweenPie)
                         ;
-
                         function tweenPie(b) {
                             var i = d3.interpolate({startAngle: 0, endAngle: 0}, b);
                             return function(t) {
                                 return arc(i(t));
                             };
                         }
-                   }
+                    }
                 });
             }
         };
